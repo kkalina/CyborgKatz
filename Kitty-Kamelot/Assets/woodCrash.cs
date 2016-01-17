2094b8bae780ccae43033cf4a526f31ffacebb1e
kkalina <kkalina@umich.edu>
2016-01-17 15:08:06 -0500

    things

---
+++ Kitty-Kamelot/Assets/woodCrash.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class woodCrash : MonoBehaviour {
+    public AudioSource clip;
+
+	// Use this for initialization
+	void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "player")
+        {
+            clip.Play();
+        }
+    }
+}
